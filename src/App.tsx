007dd568dfd353f3e25cea39cbdc1b91c2d5d8fa
codingbbq <codingbbq@gmail.com>
2024-11-17 20:52:05 +0530

    Added components, recharts and date-fns

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ import WeatherDashboard from './pages/weather-dashboard';
 import CityPage from './pages/city-page';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 60 * 5, // 5 minutes
+			gcTime: 1000 * 60 * 10, // 10 minutes
+			retry: false,
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 function App() {
 	return (
--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,7 +1,118 @@
+import CurrentWeather from '@/components/CurrentWeather';
+import HourlyTemprature from '@/components/HourlyTemprature';
+import WeatherSkeleton from '@/components/loading-skeleton';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import { useGeoLocation } from '@/hooks/useGeoLocation';
+import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from '@/hooks/useWeather';
+import { AlertCircle, AlertTriangle, MapPin, RefreshCw } from 'lucide-react';
+
 const WeatherDashboard = () => {
-    return(
-        <></>
-    )
+	const {
+		coordinates,
+		error: locationError,
+		isLoading: locationLoading,
+		getLocation,
+	} = useGeoLocation();
+
+	const weatherQuery = useWeatherQuery(coordinates);
+	const forecastQuery = useForecastQuery(coordinates);
+	const locationQuery = useReverseGeocodeQuery(coordinates);
+
+	const handleRefresh = () => {
+		getLocation();
+		if (coordinates) {
+			weatherQuery.refetch();
+			forecastQuery.refetch();
+			locationQuery.refetch();
+		}
+	};
+
+	if (locationLoading) {
+		return <WeatherSkeleton />;
+	}
+
+	if (locationError) {
+		return (
+			<Alert variant='destructive'>
+				<AlertCircle className='h-4 w-4' />
+				<AlertTitle>Location Error</AlertTitle>
+				<AlertDescription className='flex flex-col gap-4'>
+					<p>{locationError}</p>
+					<Button variant={'outline'} onClick={getLocation} className='w-fit'>
+						<MapPin className='mr-2 h-4 w-4' />
+						Enable location
+					</Button>
+				</AlertDescription>
+			</Alert>
+		);
+	}
+
+	if (!coordinates) {
+		return (
+			<Alert variant='destructive'>
+				<AlertCircle className='h-4 w-4' />
+				<AlertTitle>Coordinates Error</AlertTitle>
+				<AlertDescription className='flex flex-col gap-4'>
+					<p>Coordinates not found</p>
+					<Button variant={'outline'} onClick={getLocation} className='w-fit'>
+						<MapPin className='mr-2 h-4 w-4' />
+						Enable location
+					</Button>
+				</AlertDescription>
+			</Alert>
+		);
+	}
+
+	if (weatherQuery.error || forecastQuery.error) {
+		return (
+			<Alert variant='destructive'>
+				<AlertTriangle className='h-4 w-4' />
+				<AlertTitle>Error</AlertTitle>
+				<AlertDescription className='flex flex-col gap-4'>
+					<p>Failed to fetch weather data. Please try again.</p>
+					<Button variant='outline' onClick={handleRefresh} className='w-fit'>
+						<RefreshCw className='mr-2 h-4 w-4' />
+						Retry
+					</Button>
+				</AlertDescription>
+			</Alert>
+		);
+	}
+
+	if (!weatherQuery.data || !forecastQuery.data) {
+		return <WeatherSkeleton />;
+	}
+
+	const locationName = locationQuery.data?.[0];
+
+	return (
+		<>
+			{/* Favorite cities */}
+			<div className='flex items-center justify-between'>
+				<h1 className='text-xl font-bold tracking-tight'>My Location</h1>
+				<Button
+					variant={'outline'}
+					size={'icon'}
+					onClick={handleRefresh}
+					disabled={weatherQuery.isFetching || forecastQuery.isFetching}
+				>
+					<RefreshCw size={4} className={weatherQuery.isFetching ? 'animate-spin' : ''} />
+				</Button>
+			</div>
+			
+            <div className='grid gap-6'>
+                <div className='flex flex-col lg:flex-row gap-4'>
+                    <CurrentWeather data={weatherQuery.data} locationName={locationName } />
+                    <HourlyTemprature data={forecastQuery.data} />
+                </div>
+
+                <div>
+                    
+                </div>
+            </div>
+		</>
+	);
 };
 
-export default WeatherDashboard
+export default WeatherDashboard;
